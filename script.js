e56ec7c9438bec04598a9b512c9e638a8fee1c15
john monash <gustib722@gmail.com>
2026-09-22 00:38:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -315,8 +315,6 @@ const genreCache = getGenreCache(); // albumId -> nama genre (string) atau null
 			.sort((a, b) => b.plays - a.plays);
 	}
 
-	console.log("C1 genre preferences:", getGenrePreferences());
-
   function saveHistory(history) {
     localStorage.setItem(HISTORY_STORAGE_KEY, JSON.stringify(history));
   }
